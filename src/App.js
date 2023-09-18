f4c7b7ce8a9aaaad4ec63fcb7e87e821c04bc2ed
robertchidzugwe <robertchidzugwe@gmail.com>
2023-09-18 13:49:23 +0300

    code is okay

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MovieList from './MovieList';
 import MovieDetails from './MovieDetails';
 import WatchList from './WatchList';
-import UpcomingPage from './UpcomingPage';
+
 
 
 import AppContext from './context/AppContext';
-import TopRatedPage from './TopRatedPage';
 
 function App() {
   const [watchList,setWatchlist]=useState([])
@@ -18,11 +17,10 @@ function App() {
         <Route exact path="/" component={MovieList} />
         <Route exact path="/movie/:id" component={MovieDetails} />
         <Route path="/watchList" component={WatchList} />
-        <Route path="/upcoming" component={UpcomingPage} />
-        <Route path="/toprated" component={TopRatedPage}/>
       </Switch>
     </div>
   </Router></AppContext.Provider>  );
 }
+
 
 export default App;
--- src/UpcomingPage.js
+++ src/UpcomingPage.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import './upcoming.css';
+
+
+
+
 
 function UpcomingPage() {
   const [upcomingMovies, setUpcomingMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=f4c776b1055af7aa537d751aa6f0a329')
@@ -13,38 +14,28 @@ function UpcomingPage() {
         if (data.results && data.results.length > 0) {
           setUpcomingMovies(data.results);
         }
-        setIsLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching upcoming movies:', error);
         setUpcomingMovies([]);
-        setIsLoading(false);
       });
   }, []);
 
-  if (isLoading) {
-    return <p>Loading upcoming movies...</p>;
-  }
-
   return (
     <div>
-      <Link to="/" className="home-button">Go Home</Link>
       <h2 className='upcoming'>Upcoming Movies</h2>
-      <div className='movies-container'>
+      <ul>
         {upcomingMovies.map((movie) => (
-          <div key={movie.id} className='movie-card'>
-            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-            <div className='movie-details'>
-              <h3>{movie.title}</h3>
-              <p>{movie.overview}</p>
-              <h4 className='release'>Release Date</h4>
-              <p>{movie.release_date}</p>
-            </div>
-          </div>
+          <li key={movie.id}>{movie.title}</li>
         ))}
+      </ul>
       </div>
-    </div>
-  );
+
+
+    );
+
+   
+
 }
 
 export default UpcomingPage;
